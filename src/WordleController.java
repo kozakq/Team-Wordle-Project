8d241592e1018d20f99f17e1411dd68a57276b6f
Peli Orugbani <orugbanip@msoe.edu>
2025-02-21 10:14:59 -0600

    gui

--- src/WordleController.java
+++ src/WordleController.java
@@ -1,13 +1,18 @@
+import javafx.collections.ObservableList;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -17,6 +22,8 @@ import players.Person;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.awt.SystemColor.text;
 
 /*
  * Course: Software Tools & Process
@@ -39,6 +46,15 @@ public class WordleController {
 	private static final int MAX_GUESSES = 6;
 
 	@FXML
+	GridPane guess1;
+	@FXML
+	GridPane guess2;
+	@FXML
+	GridPane guess3;
+	@FXML
+	GridPane guess4;
+	@FXML
+	GridPane guess5;
 	VBox box;
 	@FXML
 	Label text;
@@ -46,6 +62,11 @@ public class WordleController {
 	Label GuessLabel;
 
 
+	GridPane guess6;
+	@FXML
+	TextField guessTextField;
+	private GridPane[] guessRows;
+	private int currentRow;
 	public WordleController(){
 		app = new WordleApp();
 		goalWord = app.getGoalWord();
@@ -54,11 +75,17 @@ public class WordleController {
 
 	@FXML
 	public void initialize(){
-		TextField tf = new TextField();
-		tf.setOnAction(event -> {
-			enterWord(tf.getText());
+		guessRows = new GridPane[]{guess1, guess2, guess3, guess4, guess5,guess6};
+		for (GridPane row : guessRows) {
+			for (int col = 0; col < 5; col++) {
+				Label letterLabel = new Label();
+				letterLabel.getStyleClass().add("letter-cell");
+				row.add(letterLabel, col, 0);
+			}
+		}
+		guessTextField.textProperty().addListener((observable, oldText, newText) -> {
+			updateActiveRow(newText);
 		});
-		box.getChildren().add(tf);
 	}
 	public void isGameOver() {
 		if (guessedWords != null && (guessedWords.contains(goalWord)) || guessCount == MAX_GUESSES) {
@@ -71,7 +98,7 @@ public class WordleController {
 		System.out.println(guessedWords);
 		isGameOver();
 	}
-	
+
 
 	public void changeDictionary(){
 
@@ -87,9 +114,42 @@ public class WordleController {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param word
 	 */
+	public void enterWord(String word) {
+		if (word.length() != 5) {
+			return;
+		}
+
+		if (currentRow >= guessRows.length) {
+			return;
+		}
+		GridPane activeRow = guessRows[currentRow];
+		ObservableList<Node> children = activeRow.getChildren();
+
+		for (int col = 0; col < word.length(); col++) {
+			char guessedChar = word.charAt(col);
+			Label label = (Label) children.get(col);
+			label.setText(String.valueOf(guessedChar));
+		}
+		currentRow++;
+		guessTextField.clear();
+	}
+
+	private void updateActiveRow(String text) {
+		if (currentRow >= guessRows.length) {
+			return;
+		}
+		GridPane activeRow = guessRows[currentRow];
+		ObservableList<Node> children = activeRow.getChildren();
+		for (int i = 0; i < 5; i++) {
+			Label label = (Label) children.get(i);
+			if (i < text.length()) {
+				label.setText(String.valueOf(text.charAt(i)));
+			} else {
+				label.setText("");
+			}
 	public void enterWord(String word){
 		if (word.length() == 5) {
 			boolean isValid = app.checkWord(word);
@@ -103,6 +163,7 @@ public class WordleController {
 			text.setText("Not 5 letters!");
 		}
 	}
+
 
 	public void getAllLettersGuessed(){
 
@@ -156,6 +217,13 @@ public class WordleController {
 		} catch (IOException e) {
 			System.err.println("Error opening player stats controller" + e.getMessage());
 		}
+	}
+	@FXML
+	public void show(String words) {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Mystery Word Letters");
+		alert.setContentText(words);
+		alert.showAndWait();
 	}
 	public void showEndGameWindow() {
 		Stage endGameStage = new Stage();
